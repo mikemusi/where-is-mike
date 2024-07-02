6afa89dd08bf971757746415d7327459c2417ac7
Mike Musi <mikemusi@mikes-mbp.lan>
2024-07-01 23:36:57 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordInput = document.getElementById('passwordInput');
+    const submitButton = document.getElementById('submitPassword');
+    const calendar = document.getElementById('calendar');
+    const passwordProtection = document.getElementById('passwordProtection');
+
+    const correctPassword = 'seasons'; // Updated password
+
+    submitButton.addEventListener('click', function() {
+        if (passwordInput.value === correctPassword) {
+            calendar.style.display = 'block';
+            passwordProtection.style.display = 'none';
+            initializeCalendar();
+        } else {
+            alert('Incorrect password. Please try again.');
+        }
+    });
+
+    function initializeCalendar() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            events: [
+                {
+                    title: 'New York',
+                    start: '2024-07-01',
+                    end: '2024-07-05'
+                }
+                // Add more events here
+            ]
+        });
+        calendar.render();
+    }
+});
