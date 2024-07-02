97325a755c3a12272dfdb24da46ac1e2ef22725f
mikemusi <mikemusijr@gmail.com>
2024-07-02 18:45:41 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,18 +25,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     title: 'New York',
                     start: '2024-07-01',
                     end: '2024-07-05',
-                        
-                    title: 'New York',
-                    start: '2024-07-06',
-                    end: '2024-07-10'
-                    
                 }
 
-                {
-                    title: 'New York',
-                    start: '2024-07-06',
-                    end: '2024-07-10'
-                }
                 // Add more events here
             ]
         });
