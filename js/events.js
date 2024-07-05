f2b315887e70663bcce91ffc819160e0518a05d5
mikemusi <mikemusijr@gmail.com>
2024-07-05 09:51:08 -0400

    Create events.js to simplify codebase

---
+++ js/events.js
@@ -0,0 +1,50 @@
+// events.js
+const eventData = [
+    {
+        title: 'Boston',
+        start: '2024-07-01',
+        end: '2024-07-04'
+    },
+    {
+        title: 'Vancouver',
+        start: '2024-07-05',
+        end: '2024-07-14',
+        allDay: true
+    },
+    {
+        title: 'Boston',
+        start: '2024-07-15',
+        end: '2024-08-01'
+    },
+    {
+        title: 'Cozumel',
+        start: '2024-08-02',
+        end: '2024-08-09'
+    },
+    {
+        title: 'Boston',
+        start: '2024-08-10',
+        end: '2024-09-04'
+    },
+    {
+        title: 'Palm Springs, CA',
+        start: '2024-09-05',
+        end: '2024-09-08'
+    },
+    {
+        title: 'Boston',
+        start: '2024-09-09',
+        end: '2024-09-13'
+    },
+    {
+        title: 'Boston/Zurich',
+        start: '2024-09-14',
+        end: '2024-11-03'
+    },
+    {
+        title: 'Honduras',
+        start: '2024-11-04',
+        end: '2025-01-02'
+    },
+    // Add more events here as needed
+];
